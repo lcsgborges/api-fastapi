5065c12e2e18d6d65db42fc9b915931b06145a94
lcsgborges <lcsgborges@gmail.com>
2025-07-14 01:51:51 -0300

    feat: criar schemas

--- schemas.py
+++ schemas.py
@@ -1,11 +1,67 @@
 from pydantic import BaseModel
 from typing import Optional, Text
+from datetime import date
 
 
 class AdminSchema(BaseModel):
     nome: str
     email: str
     senha: str
+    
+    class Config:
+        from_attributes = True
+        
+
+class PizzaSchema(BaseModel):
+    sabor: str
+    preco: float
+    
+    class Config:
+        from_attributes = True
+
+
+class RefrigeranteSchema(BaseModel):
+    nome: str
+    litro: float
+    preco: float
+    
+    class Config:
+        from_attributes = True
+
+
+class PedidoSchema(BaseModel):
+    data: date
+    valor: float
+    frete: float
+    pagamento: str
+    id_cliente: int
+    
+    class Config:
+        from_attributes = True
+        
+
+class ClienteSchema(BaseModel):
+    nome: str
+    telefone: str
+    endereco: Optional[Text]
+    
+    class Config:
+        from_attributes = True
+
+
+class PedidoPizzaSchema(BaseModel):
+    id_pedido: int
+    id_pizza: int
+    quntidade: int
+    
+    class Config:
+        from_attributes = True
+        
+        
+class PedidoRefrigeranteSchema(BaseModel):
+    id_pedido: int
+    id_refrigerante: int
+    quantidade: int
     
     class Config:
         from_attributes = True
